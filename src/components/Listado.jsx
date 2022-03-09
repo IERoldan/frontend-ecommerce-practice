75ef0e8981027915c56f86111f29547932c1aedc
Ivan Roldan <ivanroldan1989@gmail.com>
2022-03-09 20:06:17 -0300

    React adaptado

---
+++ src/components/Listado.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Listado = () => {
+  return (
+<ul>
+    <li>lista 1</li>
+    <li>lista 2</li>
+</ul>  )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Listado } from './components/Listado';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const element =<h1>Esto es un elemnto con react</h1>;
+const container = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Listado/>, container);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
